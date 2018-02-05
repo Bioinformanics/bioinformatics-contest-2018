05fb2e6ffb7907692121212acbf767f60306ab11
Karl <karlzhou@gmail.com>
2018-02-06 00:52:15 +0800

    test 1 hard (213/300)

--- test1/test1.py
+++ test1/test1.py
@@ -1,3 +1,4 @@
+import math
 
 def simple():
     with open('test1_input_simple.txt', 'r') as input:
@@ -14,9 +15,49 @@ def simple():
             output.write('\n')
 
 def find_atp_amount(a, b, x):
-    if b <= 3*a:
+    if b <= 3*a:  # aerobic is more economic
         return x/(a+6*b)*38
-    else:
+    else:  # fermentation is more economic
         return x/a*2
 
-simple()
+# simple()
+
+def hard():
+    with open('test1_input_hard.txt', 'r') as input:
+        lines = [line.strip() for line in input.readlines()]
+        size = int(lines[0])
+    with open('test1_output_hard.txt', 'w') as output:
+        for i in range(int(size)):
+            params = lines[i+1].split(' ')
+            a = int(params[0])
+            b = int(params[1])
+            x = int(params[2])
+            atp_amount = find_atp_amount_hard(a, b, x)
+            output.write(str(atp_amount))
+            output.write('\n')
+
+def find_atp_amount_hard(a, b, x):
+    max_apt = 0
+    unit_price_in_aerobic = a/6+b
+    max_r = math.floor(x/unit_price_in_aerobic) # r = mole of oxygen
+
+    if b <= 3*a: # aerobic is more economic, maximize oxygen
+        start = max_r
+        end = 0
+        step = -1
+    else: # fermentation is more economic, minimize oxygen
+        start = 0
+        end = max_r
+        step = 1
+    for r in range(start, end, step):
+        glucose = math.floor((x - b * r) / a)
+        glucose_in_aeribic = r/6
+        apt_amount = 38 * glucose_in_aeribic + 2 * (glucose - glucose_in_aeribic)
+        if apt_amount >= max_apt:
+            max_apt = apt_amount
+        else:
+            break
+    return max_apt
+
+# print(str(find_atp_amount_hard(7, 5, 23)))
+hard()
